1addd6e077e1cfce47448f23842acd4ce5b6aff8
ally <alwyncrasta000@gmail.com>
2020-07-16 16:27:28 +0530

    one

---
+++ precache-manifest.8f2d0ae7d52db824ce1de01a683dc4bb.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b2d0ab30991c6a2bd4375af4d50937c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "001c1f3daf5c9f409b3d",
+    "url": "/static/css/2.33f97271.chunk.css"
+  },
+  {
+    "revision": "d5ad464c761bf0b73f2f",
+    "url": "/static/css/main.a7916e85.chunk.css"
+  },
+  {
+    "revision": "001c1f3daf5c9f409b3d",
+    "url": "/static/js/2.e8e33ae0.chunk.js"
+  },
+  {
+    "revision": "30b139271c4a1eb67fb10f985b16cb53",
+    "url": "/static/js/2.e8e33ae0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5ad464c761bf0b73f2f",
+    "url": "/static/js/main.7811f9d4.chunk.js"
+  },
+  {
+    "revision": "59de543fba54edad80a8",
+    "url": "/static/js/runtime-main.49010ace.js"
+  },
+  {
+    "revision": "e4c71d42299ad816cc2ee4a9e909c452",
+    "url": "/static/media/Invoice.e4c71d42.svg"
+  },
+  {
+    "revision": "316c232187bac46e708fad0b5d83666f",
+    "url": "/static/media/Quicksand-Regular.316c2321.ttf"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/static/media/Roboto-Regular.11eabca2.ttf"
+  },
+  {
+    "revision": "0e3e08bcb441c2cb39bdfff7f56f06a2",
+    "url": "/static/media/add.0e3e08bc.svg"
+  },
+  {
+    "revision": "a137e913da7404d0bdd0723643a1b140",
+    "url": "/static/media/add_photo.a137e913.svg"
+  },
+  {
+    "revision": "ece0048e3c8a8253b16e8a69a2809387",
+    "url": "/static/media/back-arrow.ece0048e.svg"
+  },
+  {
+    "revision": "b5420402146df0c24010e56d6af98d8d",
+    "url": "/static/media/back-close.b5420402.svg"
+  },
+  {
+    "revision": "b5420402146df0c24010e56d6af98d8d",
+    "url": "/static/media/back.b5420402.svg"
+  },
+  {
+    "revision": "f0d884b14ef82622d11f246f50427cef",
+    "url": "/static/media/chatbox.f0d884b1.svg"
+  },
+  {
+    "revision": "b840e171a500c69863f00f6b4056b486",
+    "url": "/static/media/close.b840e171.svg"
+  },
+  {
+    "revision": "9c2cef916838953f3d4e047e389224f2",
+    "url": "/static/media/coupons.9c2cef91.svg"
+  },
+  {
+    "revision": "1a676ea9f7aacde6be4d7d0da84ee41a",
+    "url": "/static/media/danger.1a676ea9.svg"
+  },
+  {
+    "revision": "e51578ba43002a7c89492115bc131119",
+    "url": "/static/media/dashboard.e51578ba.svg"
+  },
+  {
+    "revision": "3147dcb7f8937668f92f0e6991be3b5b",
+    "url": "/static/media/delete-.3147dcb7.svg"
+  },
+  {
+    "revision": "80c51572a0f52bfdb530fcf1183076a2",
+    "url": "/static/media/description.80c51572.svg"
+  },
+  {
+    "revision": "3a9ed45d3fa5ad71de06e06aedbae751",
+    "url": "/static/media/eye-close.3a9ed45d.svg"
+  },
+  {
+    "revision": "e5b7cb1c1cd4d09f2026c9734429b40f",
+    "url": "/static/media/eye.e5b7cb1c.svg"
+  },
+  {
+    "revision": "a92f85a49ed132dde1b924e74cc618f9",
+    "url": "/static/media/info.a92f85a4.svg"
+  },
+  {
+    "revision": "d1c1ebfb954222293ae60a88b42b2a9b",
+    "url": "/static/media/logo.d1c1ebfb.png"
+  },
+  {
+    "revision": "55f6849429de279d473c459c7ead3249",
+    "url": "/static/media/menu.55f68494.svg"
+  },
+  {
+    "revision": "73d85ecce6f23543d44216b28af5c4da",
+    "url": "/static/media/notifications.73d85ecc.svg"
+  },
+  {
+    "revision": "b4935e81ab80c59be0a096bf87f6a33c",
+    "url": "/static/media/orders.b4935e81.svg"
+  },
+  {
+    "revision": "11390431850070ff08741f6afa055e44",
+    "url": "/static/media/payment.11390431.svg"
+  },
+  {
+    "revision": "9ef54d34b92f39fc25b8076840497feb",
+    "url": "/static/media/quest.9ef54d34.svg"
+  },
+  {
+    "revision": "6b9282ccf035f6f1fbe9cadada9fd647",
+    "url": "/static/media/reviews.6b9282cc.svg"
+  },
+  {
+    "revision": "d6831f9f47606663bbab23c80e7d85cf",
+    "url": "/static/media/search.d6831f9f.svg"
+  },
+  {
+    "revision": "2d772aa01283a1dcdb3d3af46509e539",
+    "url": "/static/media/store.2d772aa0.svg"
+  }
+]);
